f63347e6710f572e32a66ff10af6bd218cd071f9
Luana Barros <dev.luana@gmail.com>
2020-11-18 09:32:38 -0300

    wew

--- run_clients.sh
+++ run_clients.sh
@@ -1,4 +1,10 @@
-for i in `seq 1 10`; do
-    # echo "conectando cliente: $i"
+pids=""
+for i in {1..10}
+do
     ./cliente 127.0.0.1 1024 &
+    pids="$pids $!"
+done
+
+for pid in $pids; do
+    wait $pid
 done
