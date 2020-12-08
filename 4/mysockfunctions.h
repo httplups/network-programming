88fe4a8939c466f0d4bb2cad51d8da33991ac8d5
Luana Barros <dev.luana@gmail.com>
2020-12-08 14:51:01 -0300

    dsdsds

--- 4/mysockfunctions.h
+++ 4/mysockfunctions.h
@@ -138,8 +138,11 @@ char *sock_ntop(const struct sockaddr *sa, socklen_t salen)
     return NULL;
 }
 
-int Select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict errorfds,struct timeval *restrict timeout) {
-    return select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict errorfds, struct timeval *restrict timeout);
+int Select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict errorfds,
+struct timeval *restrict timeout) {
+    int res = select(int nfds, fd_set *restrict, fd_set *restrict, fd_set *restrict,
+         struct timeval *restrict);
+    return res;
 }
 
 
