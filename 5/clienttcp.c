232aacad2f3c80afb546b2bae7335d55291382df
Luana Barros <dev.luana@gmail.com>
2020-12-29 18:03:29 -0300

    aa

--- 5/clienttcp.c
+++ 5/clienttcp.c
@@ -67,7 +67,19 @@ int main(int argc, char **argv)
     printf("Welcome to the game!\n");
     /*  ================================================================================== */
     /* USING SELECT TO LISTEN ON BOTH SOCKETS */
+    fd_set rset;
+
+    FD_ZERO(&rset);
+
     do {
+
+        /* Associando file descriptors ao conjunto read*/
+        FD_SET(socktcp, &rset);
+        FD_SET(sockudp, &rset);
+        
+        maxfdp1 = max(socktcp, sockudp)  +  1;
+        Select(maxfdp1,  &rset,  NULL,  NULL,  NULL);
+
         printf("\n\nChoose one option below:\n\n1 - Invite someone to play with\n0 - Quit\n");
         scanf(" %d", &option);
 
