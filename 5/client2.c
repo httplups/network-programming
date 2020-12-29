24161e88ef117368181a2cd643d31b61b563570a
Luana Barros <dev.luana@gmail.com>
2020-12-29 18:27:06 -0300

    a

--- 5/client2.c
+++ 5/client2.c
@@ -10,8 +10,9 @@ struct sockaddr_in servtcpaddr, servudpaddr, clitcpaddr, cliudpaddr;
 
 
 void *tcp_client(void *p) {
-    int server_port_number, socktcp;
+    int server_port_number, socktcp, option, ID;
     char **argv = p;
+    char ID_str[100], players[MAXLINE], player_port[MAXLINE];
     socklen_t lencli;
     
     /* CONNECTING TO A TCP SERVER FIRST */
@@ -42,6 +43,57 @@ void *tcp_client(void *p) {
     printf("connected: %s:%d\n", inet_ntoa(clitcpaddr.sin_addr), ntohs(clitcpaddr.sin_port));
     // printf("done: %s:%d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
 
+
+    do {
+
+        printf("\n\nChoose one option below:\n\n1 - Invite someone to play with\n0 - Quit\n");
+        scanf(" %d", &option);
+
+        switch (option) {
+            case 1: {
+                char * get = "get";
+                Write(socktcp, get, strlen(get));
+                Read(socktcp, players, MAXLINE);
+                
+
+                if (strcmp(players, "NULL") == 0) {
+                    printf("No players avaiable. Try again soon...");
+                    continue;
+                }
+                
+                printf("============ List of players: ============\n");
+                printf("\nID\tIP\tPort\n");
+                printf("%s\n", players);
+
+                printf("Choose one player to play with by ID:");
+                scanf(" %d", &ID);
+                printf("You chose %d\n", ID);
+
+                memset(ID_str, 0, strlen(ID_str));
+                sprintf(ID_str, "%d", ID);
+                // printf("str: %s\n",ID_str);
+                Write(socktcp, ID_str, strlen(ID_str));
+
+                // /* getting port of player assuming it's listening on 0.0.0.0 */
+                // Read(socktcp, player_port, MAXLINE);
+                // printf("Port: %s\n", player_port);
+                
+                // char delim[] = " ";
+                // char *ptr = strtok(player, delim);
+                // printf("IP:%s\n", ptr);
+                // ptr = strtok(NULL, delim);
+                // printf("Port:%s\n", ptr);
+                
+            }
+            case 0:
+                //finish conn
+                break;
+        }
+
+    } while(option != 0);
+
+    pthread_exit(NULL);
+
 }
 
 void *udp_server(void *p) {
@@ -50,8 +102,7 @@ void *udp_server(void *p) {
 
 int main(int argc, char **argv)
 {
-    char ID_str[100], players[MAXLINE], player_port[MAXLINE];
-    int socktcp, sockudp, server_port_number, option, ID, maxfdp1;
+    
 	
 
     if (argc != 3)
