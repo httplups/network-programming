eea7baeec4a4d3c4334bdb57a86a0bba5bba95de
Luana Barros <dev.luana@gmail.com>
2020-12-29 18:23:23 -0300

    a

--- 5/client2.c
+++ 5/client2.c
@@ -6,13 +6,42 @@
 
 #define MAXLINE 4096
 pthread_t thread1, thread2;
+struct sockaddr_in servtcpaddr, servudpaddr, clitcpaddr, cliudpaddr;
 
 
 void *tcp_client(void *p) {
-    int server_port_number;
+    int server_port_number, socktcp;
     char **argv = p;
+    socklen_t lencli;
+    
+    /* CONNECTING TO A TCP SERVER FIRST */
+    /* Creating socket */
+    socktcp = Socket(AF_INET, SOCK_STREAM, 0);
+
+    // Initializing
+    bzero(&servtcpaddr, sizeof(servtcpaddr));
+    bzero(&clitcpaddr, sizeof(clitcpaddr));
+
+    servtcpaddr.sin_family = AF_INET;
+
+    // obtendo o numero de porta para se conectar ao servidor
     sscanf(argv[2], "%d", &server_port_number);
-    printf("arg: %d\n", server_port_number);
+    servtcpaddr.sin_port = htons(server_port_number);
+
+    //obtendo o IP para se conectar ao servidor
+    Inet_pton(AF_INET, argv[1], &servtcpaddr.sin_addr);
+
+    /* Connecting to the server */
+    Connect(socktcp, (struct sockaddr *)&servtcpaddr, sizeof(servtcpaddr));
+
+    lencli = sizeof(clitcpaddr);
+
+    // Agora que a conexão foi feita, o connect implicitamente chamou o método bind e associou ao socket um numero de porta e ip local
+    GetSockName(socktcp, (struct sockaddr *)&clitcpaddr, &lencli);
+    // printf("Informacoes do Socket Local:\n");
+    printf("connected: %s:%d\n", inet_ntoa(clitcpaddr.sin_addr), ntohs(clitcpaddr.sin_port));
+    // printf("done: %s:%d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+
 }
 
 void *udp_server(void *p) {
@@ -21,10 +50,9 @@ void *udp_server(void *p) {
 
 int main(int argc, char **argv)
 {
-    char error[MAXLINE + 1], ID_str[100], players[MAXLINE], player_port[MAXLINE];
+    char ID_str[100], players[MAXLINE], player_port[MAXLINE];
     int socktcp, sockudp, server_port_number, option, ID, maxfdp1;
-	struct sockaddr_in servtcpaddr, servudpaddr, clitcpaddr, cliudpaddr;
-	socklen_t lencli;
+	
 
     if (argc != 3)
 	{
