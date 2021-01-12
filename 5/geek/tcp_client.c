6a4dea373602bce0a82f7b9b684fefa6c9d5527e
Luana Barros <dev.luana@gmail.com>
2021-01-12 11:34:38 -0300

    a

--- 5/geek/tcp_client.c
+++ 5/geek/tcp_client.c
@@ -1,9 +1,11 @@
 
 // TCP Client program 
-#include <netinet/in.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>  
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
+#include <unistd.h>
 #include <sys/socket.h> 
 #include <sys/types.h> 
 #define PORT 5000 
