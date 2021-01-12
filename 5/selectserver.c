0777f4a6622e06f85129653e0e2ec2c88729947b
Luana Barros <dev.luana@gmail.com>
2021-01-12 11:50:07 -0300

    timeout

--- 5/selectserver.c
+++ 5/selectserver.c
@@ -1,5 +1,6 @@
 #include "mysockfunctions.h"
 // #define FD_SETSIZE 10
+#include <sys/time.h>       /* for struct timeval {} */
 
 int n_users = 0;
 
@@ -125,6 +126,7 @@ int main(int argc, char **argv) {
     char    buf[MAXLINE], resp[MAXLINE], otherclient[100], player[100];
     socklen_t  clilen;
     struct sockaddr_in cliaddr, servaddr;
+    struct timeval selTimeout;       /* Timeout for select() */
 
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
@@ -146,128 +148,118 @@ int main(int argc, char **argv) {
     //     client[i] = -1;          /* -1 indicates available entry */
     initialize_clients();
     
-    FD_ZERO(&allset);
-    FD_SET(listenfd, &allset);
+    
     for ( ; ; ) {
+        FD_ZERO(&allset);
+        FD_SET(listenfd, &allset);
         printf("Listening...\n");
+
+        selTimeout.tv_sec = 3;       /* timeout (secs.) */
+        selTimeout.tv_usec = 0;            /* 0 microseconds */
         
         
         rset = allset;          /* structure assignment */
-        nready = Select(maxfd + 1, &rset, NULL, NULL, NULL);
+        if(Select(maxfd + 1, &rset, NULL, NULL, &selTimeout) == 0) 
+            printf("No echo requests for %ld secs...Server still alive\n", timeout);
+        else {
+
         
-        if (FD_ISSET(listenfd, &rset)) {       /* new client connection */
-            clilen = sizeof(cliaddr);
-            // connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
-            connfd = Accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+            if (FD_ISSET(listenfd, &rset)) {       /* new client connection */
+                clilen = sizeof(cliaddr);
+                connfd = Accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+                GetPeerName(connfd, (struct sockaddr *)&cliaddr, &clilen);
+                i = insert_client(inet_ntoa(cliaddr.sin_addr),ntohs(cliaddr.sin_port), connfd);
+                if (i == FD_SETSIZE) {
+                    printf("too many clients");
+                    exit(1);
+                }
 
-            // char username[15], otheruser[15];
-            // memset(username, 0, strlen(username));
-            // memset(otheruser, 0, strlen(otheruser));
+                FD_SET(connfd, &allset);       /* add new descriptor to set */
 
-            // Write(connfd, welcome, strlen(welcome));
-            // Read(connfd, username, 15);
-            // username[strlen(username) -1] = 0;
-            GetPeerName(connfd, (struct sockaddr *)&cliaddr, &clilen);
-            i = insert_client(inet_ntoa(cliaddr.sin_addr),ntohs(cliaddr.sin_port), connfd);
-            
-            // for (i = 0; i < FD_SETSIZE; i++)
-            //     if (client[i] < 0) {
-            //         client[i] = connfd; /* save descriptor */
-            //         break;
-            //     }
+                if (connfd > maxfd)
+                    maxfd = connfd; /* for select */
+                if (i > maxi)
+                    maxi = i;          /* max index in client[] array */
 
-            
-            // i = insert_client_socket(connfd);
-            if (i == FD_SETSIZE) {
-                printf("too many clients");
-                exit(1);
-            }
-
-            FD_SET(connfd, &allset);       /* add new descriptor to set */
-
-            if (connfd > maxfd)
-                maxfd = connfd; /* for select */
-            if (i > maxi)
-                maxi = i;          /* max index in client[] array */
-
-            if (--nready <= 0)
-                continue;          /* no more readable descriptors */
-            
-        }
-        
-        for (i = 0; i <= maxi; i++) {       /* check all clients for data */
-
-
-            if ( (sockfd = clients[i].socket_conn) < 0) /* check connection socket for each client */
-                continue;
-
-            if (FD_ISSET(sockfd, &rset)) {
-                memset(buf, 0, strlen(buf));
-                memset(player, 0, strlen(player));
-                if ( (n = Read(sockfd, buf, MAXLINE)) == 0) {
-                    /* connection closed by client */
-                    Close(sockfd);
-                    FD_CLR(sockfd, &allset);
-                    clients[i].socket_conn = -1;
-
-                } 
-                else{
-                    printf("got from client: %s\n", buf);
-
-                    if (strcmp(buf, "get") == 0) {
-                        /* send the players available */
-                        memset(resp, 0, strlen(resp));
-                        memset(otherclient, 0, strlen(otherclient));
-
-                        strcpy(resp, show_other_players(sockfd));
-                        Write(sockfd, resp, strlen(resp));
-
-                        Read(sockfd, otherclient, 100); /*this call is blocking*/
-                        // printf("Id of other: %s\n", otherclient);
-
-                        if (atoi(otherclient) == -1)
-                            continue;                         
-
-                        /* Sending port of client */
-                        memset(resp, 0, strlen(resp));
-                        strcpy(resp, get_info_player(atoi(otherclient)));
-                        printf("resp %s\n", resp);
-                        Write(sockfd, resp, strlen(resp));
-
-                        // set_as_offline(i);
-                        // set_as_offline(otherclient);
-
-                    }
-                    else if (strcmp(buf, "back") == 0) {
-                        /* Put the client again on the array */
-                        // memset(player, 0, strlen(player));
-
-                        /* Getting the port number of the player playing with client i */
-                        Read(sockfd, player, 100);
-                        // printf("porta player:%s\n", player);
-                        set_as_online(i);
-                        set_as_online(get_index_by_port(atoi(player)));
-                        show_clients();
-                    }
-                    else if (strcmp(buf, "playing") == 0) {
-
-                        /* Getting the port number of the player playing with client i */
-                        Read(sockfd, player, 100);
-                        // printf("porta player:%s\n", player);
-                        set_as_offline(i);
-                        set_as_offline(get_index_by_port(atoi(player)));
-                        show_clients();
-                    }
-                    else if (strcmp(buf, "points") == 0) {
-                        continue;
-                    }
-                    // Write(sockfd, buf, n);
-                }                   
-                   
-                
                 if (--nready <= 0)
-                    break;         /* no more readable descriptors */
+                    continue;          /* no more readable descriptors */
+                
             }
-        } 
+        
+            // for (i = 0; i <= maxi; i++) {       /* check all clients for data */
+
+
+            //     if ( (sockfd = clients[i].socket_conn) < 0) /* check connection socket for each client */
+            //         continue;
+
+            //     if (FD_ISSET(sockfd, &rset)) {
+            //         memset(buf, 0, strlen(buf));
+            //         memset(player, 0, strlen(player));
+            //         if ( (n = Read(sockfd, buf, MAXLINE)) == 0) {
+            //             /* connection closed by client */
+            //             Close(sockfd);
+            //             FD_CLR(sockfd, &allset);
+            //             clients[i].socket_conn = -1;
+
+            //         } 
+            //         else{
+            //             printf("got from client: %s\n", buf);
+
+            //             if (strcmp(buf, "get") == 0) {
+            //                 /* send the players available */
+            //                 memset(resp, 0, strlen(resp));
+            //                 memset(otherclient, 0, strlen(otherclient));
+
+            //                 strcpy(resp, show_other_players(sockfd));
+            //                 Write(sockfd, resp, strlen(resp));
+
+            //                 Read(sockfd, otherclient, 100); /*this call is blocking*/
+            //                 // printf("Id of other: %s\n", otherclient);
+
+            //                 if (atoi(otherclient) == -1)
+            //                     continue;                         
+
+            //                 /* Sending port of client */
+            //                 memset(resp, 0, strlen(resp));
+            //                 strcpy(resp, get_info_player(atoi(otherclient)));
+            //                 printf("resp %s\n", resp);
+            //                 Write(sockfd, resp, strlen(resp));
+
+            //                 // set_as_offline(i);
+            //                 // set_as_offline(otherclient);
+
+            //             }
+            //             else if (strcmp(buf, "back") == 0) {
+            //                 /* Put the client again on the array */
+            //                 // memset(player, 0, strlen(player));
+
+            //                 /* Getting the port number of the player playing with client i */
+            //                 Read(sockfd, player, 100);
+            //                 // printf("porta player:%s\n", player);
+            //                 set_as_online(i);
+            //                 set_as_online(get_index_by_port(atoi(player)));
+            //                 show_clients();
+            //             }
+            //             else if (strcmp(buf, "playing") == 0) {
+
+            //                 /* Getting the port number of the player playing with client i */
+            //                 Read(sockfd, player, 100);
+            //                 // printf("porta player:%s\n", player);
+            //                 set_as_offline(i);
+            //                 set_as_offline(get_index_by_port(atoi(player)));
+            //                 show_clients();
+            //             }
+            //             else if (strcmp(buf, "points") == 0) {
+            //                 continue;
+            //             }
+            //             // Write(sockfd, buf, n);
+            //         }                   
+                    
+                    
+            //         if (--nready <= 0)
+            //             break;         /* no more readable descriptors */
+            //     }
+            // } 
+        }
     }
 }   
