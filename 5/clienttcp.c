566d992fbdb58d9014704e06946ba99986d276d5
Luana Barros <dev.luana@gmail.com>
2021-01-12 14:35:35 -0300

    a

--- 5/clienttcp.c
+++ 5/clienttcp.c
@@ -5,6 +5,20 @@
 
 #define MAXLINE 4096
 
+char* itoa(int val, int base){
+	
+	static char buf[32] = {0};
+	
+	int i = 30;
+	
+	for(; val && i ; --i, val /= base)
+	
+		buf[i] = "0123456789abcdef"[val % base];
+	
+	return &buf[i+1];
+	
+}
+
 int main(int argc, char **argv)
 {
     char error[MAXLINE + 1], ID_str[100], players[MAXLINE], buffer[MAXLINE], sendline[MAXLINE];
